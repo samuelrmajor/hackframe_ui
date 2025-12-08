774dcf5593c06f2d970a7b0b145d5d3dc8aef5cc
Sam Major <samuelrmajor@gmail.com>
2025-12-08 16:24:03 -0500

    misc widget

--- src/components/widgets/MiscWidget.tsx
+++ src/components/widgets/MiscWidget.tsx
@@ -5,12 +5,11 @@ import Card from "./Card";
 
 
 export default function MiscWidget() {
-  return (
-    <Card>
-      <div className="text-gray-300 text-sm mb-1">Daily Quote</div>
-      <div className="text-white text-lg">“Stay hard.”</div>
-      <div className="text-gray-400 text-xs mt-1">— Goggins</div>
-    </Card>
-  );
+
+    return (
+        <div>
+            Misc Widget
+        </div>
+    );
 }
 
